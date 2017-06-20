e49d383905adf0787176b5c016d6f6f3207c5047
MeriemLarbi <meriemismahlarbi@gmail.com>
2017-06-20 11:43:49 +0200

    pad

--- ft_padandprint.c
+++ ft_padandprint.c
@@ -6,36 +6,85 @@
 /*   By: ilarbi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/08 14:22:07 by ilarbi            #+#    #+#             */
-/*   Updated: 2017/06/16 14:24:49 by ilarbi           ###   ########.fr       */
+/*   Updated: 2017/06/20 11:43:14 by ilarbi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include "ft_printf.h"
+
+int		size(char *str)
+{
+	return ((*str == '-' || *str == '+') ? (ft_strlen(str) - 1)
+			: (ft_strlen(str)));
+}
+
+void	ft_prepend(char c, int nbr, int *written)
+{
+	int		i;
+
+	i = 0;
+	if (c == 'x')
+		write(1, "0x", (nbr = 2));
+	else if (c == 'X')
+		write(1, "0X", (nbr = 2));
+	else if (ft_tolower(c) == 'o')
+		write(1, "0", (nbr = 1));
+	else
+		while (i++ < nbr)
+			write(1, &c, 1);
+	*written += nbr;
+}
 
 void	ft_padandprint(char **str, t_format **f, int *written)
 {
 	int		fldsize;
-	char	signtype;
+	char	sign;
+	int		o;
+	char	pad[2];
 
-	fldsize = 0;
-	signtype = ' ';
-	if (!str || !f || !*str || !*f || !written)
+	printf("test \n");
+	if (!*str || !*f || !written)
 		return ;
-	if (ft_issigned(*((*f)->type)) && (*f)->width && !((*f)->width->max)
-		&& *(*str) == '0')
+	ft_strcpy(pad, "r ");
+	o = ((*((*f)->type) == 'o' && (*f)->flags && (*f)->flags->hash) ? 1 : 0);
+	fldsize = size(*str);
+	if ((*f)->width && (*f)->width->max > fldsize)
+		fldsize = (*f)->width->max;
+	if ((*f)->width && (*f)->width->min > fldsize)
+	{	if ((*f)->flags && ((*f)->flags->zero || (*f)->flags->minus))
+			(((*f)->flags->minus) ? ft_strcpy(pad, "l ") : ft_strcpy(pad, "r0"));
+		if (pad[0] == 'r')
 		{
-			ft_memdel(f);
-			ft_strdel(str);
-			return ;
+			ft_prepend(pad[1], (*f)->width->min - fldsize - o, written);
+			printf("\npad with : [%c] minpad %d\n", pad[1], (*f)->width->min - fldsize);
 		}
-	if (ft_issigned(*((*f)->type)))
-		fldsize = max;
-	if (f->flags->hash)
-		signtype = *((*f)->type);
-	else if (*(*str) == '-')
-		signtype = '-';
-	else if (f->flags && f->flags->plus)
-		signtype = '+';
-	ft_putstr(str);
-	*written += *written + ft_strlen(str);
+	}
+	if (*(*str) == '-' || ((*f)->flags && ((*f)->flags->plus || (*f)->flags->space)))
+	{
+		((*(*str) == '-') ? (sign = '-') : 0);
+		(((*f)->flags->plus) ? (sign = '+') : (sign = ' '));
+		ft_prepend(sign, 1, written);
+		fldsize++;
+	}
+	if ((*f)->flags && (*f)->flags->hash)
+		(*((*f)->type) == 'o' && (o = 1)) ? ft_prepend(*((*f)->type), 1, written)
+		: ft_prepend(*((*f)->type), 1, written);
+	if (fldsize > size(*str) + o)
+		ft_prepend('0', fldsize - size(*str) - o, written); 
+	((*(*str) == '-') ? write(1, *str + 1, ft_strlen(*str)) :
+	 write(1, *str, ft_strlen(*str)));//printwstr
+	if ((*f)->width && (*f)->width->min > fldsize && pad[0] == 'l')
+		ft_prepend(pad[1], (*f)->width->min - fldsize - o, written);
 }
+/*
+int		main()
+{
+	int written = 0;
+	char str = ' ';
+	ft_prepend(str, 5, &written);
+	printf("%d\n", written);
+	//printf("%-4d\n", size(str));
+	return (0);
+}
+*/
